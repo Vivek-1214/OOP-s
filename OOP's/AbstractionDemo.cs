1b3638ce595b078ea8102082bdf87315a915f35c
deepak shinde <deepak shinde@DELL>
2023-03-03 14:56:40 +0530

    abstraction

---
+++ OOP's/AbstractionDemo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Abstracttion
+{
+    internal abstract class AbstractionDemo  //Abstract class
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public abstract int sub(int x,int y);             //we cannot give body to abstract method   
+    }
+
+    interface IinterfaceDemo         //Interface
+    {
+       public  void mul(int a,int b);
+       public  void div(int x,int y);
+
+        public void rem(int m,int n);
+    }
+
+   /* class child :   AbstractionDemo
+    {
+
+        public override  int sub(int x,int y)
+        {
+            return x - y;
+        }
+    }*/
+
+   public class child1 : IinterfaceDemo
+    {
+      public void mul(int a,int b)
+        {
+            int c = a * b;
+            Console.WriteLine("multiplication is = "+c);
+        }
+       public  void div(int x,int y)
+        {
+            int c = x / y;
+            Console.WriteLine("division is = "+c);
+        }
+
+      public   void rem(int m, int n)
+        {
+            int c = m % n;
+            Console.WriteLine("reminder is = " + c);
+        }
+
+    }
+
+
+}
--- OOP's/Program.cs
+++ OOP's/Program.cs
@@ -1,44 +1,46 @@
-﻿using OOP_Employees;
+﻿using OOP_Abstracttion;
+using OOP_Employees;
 using OOP_Student;
 using System;
 using OOP_s.F1;
 namespace OOP_Concepts
-{   class TypesOfMethods
+{
+    class TypesOfMethods
     {
         //non-values return methood without parameter
 
-        public void Add()
-        {   
-            int a=10, b=15, c;
-            c = a + b;
-            Console.WriteLine(c);
-        }
-        //non-values return methood with parameter
-        public void Add01(int a,int b,out int c)             //types of parameters
+       /* public void Add()
         {
-           
+            int a = 10, b = 15, c;
+            c = a + b;
+            Console.WriteLine(c);*/
+        //non-values return methood with parameter
+       /* public void Add01(int a, int b, out int c)             //types of parameters
+        {
+
             c = a + b;
             Console.WriteLine(c);
-        }
+        }*/
 
         //values return methood without parameter
 
-        public  int Sub()
-        { int a = 5, b = 3;
-
-            return a - b;
-        }
-        //values return methood with parameter
-        public int Sub01(int a=5,int b=6)                  //parameter with default value
+      /*  public int Sub()
         {
-           
+            int a = 5, b = 3;
 
             return a - b;
-        }
+        }*/
+        //values return methood with parameter
+       /* public int Sub01(int a = 5, int b = 6)                  //parameter with default value
+        {
+
+
+            return a - b;
+        }*/
     }
     class program
     {
-       static void Main(String[] args)
+        static void Main(String[] args)
         {
             /*  TypesOfMethods T = new TypesOfMethods();
               ParaConDemo Para = new ParaConDemo(85);          //we can give parameter to the explicite constructor
@@ -49,69 +51,75 @@ namespace OOP_Concepts
               // T.Add01(45,5);
               //Console.WriteLine(T.Sub());
               // Console.WriteLine(T.Sub01(2,1));
-
               // Console.WriteLine(U.Id);
               // U.Fullname();*/
 
 
             //Class1 c1 = new Class1();
             //c1.Add();
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                                    // Employee//
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // Employee//
 
-            Employee Id;        // class variable
+          //  Employee Id;        // class variable
 
-           /* Employee Employee01 = new Employee();   //objectb of class
-            Employee01.Id = 1;
-            Employee01.Name  = "vivek";
-            Employee01.Gender = "male";
+            /* Employee Employee01 = new Employee();   //objectb of class
+             Employee01.Id = 1;
+             Employee01.Name  = "vivek";
+             Employee01.Gender = "male";
+             Console.WriteLine("Employee1 id = " + Employee01.Id);
+             Console.WriteLine("Employee1 name = " + Employee01.Name);
+             Console.WriteLine("Employee1 gender = " + Employee01.Gender);
+             Console.WriteLine("-------------------------------------------------------------------------------");
+             Employee Employee02 = new Employee();
+             Console.WriteLine("enter employee Id ");
+             Employee02.Id =Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("enter employee Name ");
+             Employee02.Name  = Console.ReadLine();
+             Console.WriteLine("enter employee Gender ");
+             Employee02.Gender  = Console.ReadLine();
+             Console.WriteLine("Employee2 id = " + Employee02.Id);
+             Console.WriteLine("Employee2 name = " + Employee02.Name);
+             Console.WriteLine("Employee2 gender = " + Employee02.Gender);
+             Console.WriteLine("-------------------------------------------------------------------------------");
+             Employee Employee03= new Employee();
+             Employee03.GetData();
+             Employee03.ShowData();
+             Console.WriteLine("-------------------------------------------------------------------------------");*/
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //STUDENTS//
 
-            Console.WriteLine("Employee1 id = " + Employee01.Id);
-            Console.WriteLine("Employee1 name = " + Employee01.Name);
-            Console.WriteLine("Employee1 gender = " + Employee01.Gender);
-            Console.WriteLine("-------------------------------------------------------------------------------");
+            /*  Students students1 = new Students();
+              students1.GetData();
+              students1.ShowData();
+              Console.WriteLine("-------------------------------------------------------------------------------");
+              Students students2 = new Students();
+              students1.GetData();
+              students1.ShowData();
+              Console.WriteLine("-------------------------------------------------------------------------------");
+              Students students3 = new Students();
+              students1.GetData();
+              students1.ShowData();
+              Console.WriteLine("-------------------------------------------------------------------------------");
+              Students students4 = new Students();
+              students1.GetData();
+              students1.ShowData();
+              Console.WriteLine("-------------------------------------------------------------------------------");*/
 
-            Employee Employee02 = new Employee();
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //ABSTRACTION//
 
-            Console.WriteLine("enter employee Id ");
-            Employee02.Id =Convert.ToInt32(Console.ReadLine());
+            // AbstractionDemo A = new AbstractionDemo();       //we cannot create the object of abstract class
 
-            Console.WriteLine("enter employee Name ");
-            Employee02.Name  = Console.ReadLine();
+           /* child c = new child();
+            Console.WriteLine(c.add(10,5));
+            Console.WriteLine(c.sub(10,5));*/
 
-            Console.WriteLine("enter employee Gender ");
-            Employee02.Gender  = Console.ReadLine();
+            // IinterfaceDemo I = new IinterfaceDemo();            // we cannot create the object of Interface
 
-            Console.WriteLine("Employee2 id = " + Employee02.Id);
-            Console.WriteLine("Employee2 name = " + Employee02.Name);
-            Console.WriteLine("Employee2 gender = " + Employee02.Gender);
-            Console.WriteLine("-------------------------------------------------------------------------------");
-
-            Employee Employee03= new Employee();
-            Employee03.GetData();
-            Employee03.ShowData();
-            Console.WriteLine("-------------------------------------------------------------------------------");*/
- //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                                                                //STUDENTS//
-
-            Students students1 = new Students();
-            students1.GetData();
-            students1.ShowData();
-            Console.WriteLine("-------------------------------------------------------------------------------");
-            Students students2 = new Students();
-            students1.GetData();
-            students1.ShowData();
-            Console.WriteLine("-------------------------------------------------------------------------------");
-            Students students3 = new Students();
-            students1.GetData();
-            students1.ShowData();
-            Console.WriteLine("-------------------------------------------------------------------------------");
-            Students students4 = new Students();
-            students1.GetData();
-            students1.ShowData();
-            Console.WriteLine("-------------------------------------------------------------------------------");
-                ;
-
+            child1 c1 = new child1();
+            c1.mul(10, 2);
+            c1.div (10, 2);
+            c1.rem (10, 2);
         }
 
 
